a8b362f26a68684095bd8c26979b604a2ee05651
Kristian Virkus <kristian.virkus@gmail.com>
2015-09-20 23:03:21 +0200

    Implemented an extended switch-case with callbacks using a cases list.

---
+++ src/Foundations/Foundations.UnitTests/Flow/SwitchTest.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elements.Foundations.Flow;
+using NUnit.Framework;
+using FluentAssertions;
+using System.Threading;
+
+namespace Foundations.UnitTests.Flow
+{
+	[TestFixture]
+	public class SwitchTest
+	{
+		[Test]
+		public void IntegerNullCases_Should_ReturnFalse()
+		{
+			Switch.Evaluate(7, null).Should().BeFalse();
+		}
+
+		[Test]
+		public void IntegerEmptyCases_Should_ReturnFalse()
+		{
+			Switch.Evaluate(7, new Switch.SwitchCase<int>[0]).Should().BeFalse();
+		}
+
+		[Test]
+		public void IntegerOneCaseFirstMatching_Should_ReturnTrue()
+		{
+			Switch.Evaluate(7, new[] { Switch.Case<int>(v => v == 7, (value) => { return false; }) }).Should().BeTrue();
+		}
+
+		[Test]
+		public void IntegerOneCaseNoneMatching_Should_NotInvokeCallbackReturnFalse()
+		{
+			var invoked = false;
+			Switch.Evaluate(7, new[] { Switch.Case<int>(v => v == 8, (value) => { invoked = true; return true; }) }).Should().BeFalse();
+			invoked.Should().BeFalse();
+		}
+
+		[Test]
+		public void IntegerOneCaseFirstMatching_Should_InvokeCallbackAndReturnTrue()
+		{
+			var invoked = false;
+			Switch.Evaluate(7, new[] { Switch.Case<int>(v => v == 7, (value) => { invoked = true; return true; }) }).Should().BeTrue();
+			invoked.Should().BeTrue();
+		}
+
+		[Test]
+		public void IntegerTwoCasesFirstMatching_Should_InvokeOnlyOneCallbackAndReturnTrue()
+		{
+			var invoked1 = false;
+			var invoked2 = false;
+			Switch.Evaluate(7, new[]
+			{
+				Switch.Case<int>(v => v == 7, (value) => { invoked1 = true; return true; }),
+				Switch.Case<int>(v => v == 8, (value) => { invoked2 = true; return true; }),
+			}).Should().BeTrue();
+			invoked1.Should().BeTrue();
+			invoked2.Should().BeFalse();
+		}
+
+		[Test]
+		public void IntegerTwoCasesSecondMatching_Should_InvokeOnlyOneCallbackAndReturnTrue()
+		{
+			var invoked1 = false;
+			var invoked2 = false;
+			Switch.Evaluate(7, new[]
+			{
+				Switch.Case<int>(v => v == 6, (value) => { invoked1 = true; return true; }),
+				Switch.Case<int>(v => v == 7, (value) => { invoked2 = true; return true; }),
+			}).Should().BeTrue();
+			invoked1.Should().BeFalse();
+			invoked2.Should().BeTrue();
+		}
+
+		[Test]
+		public void IntegerTwoCasesFirstAndSecondMatchingAndReturningFalse_Should_InvokeBothCallbacksAndReturnTrue()
+		{
+			var invoked1 = false;
+			var invoked2 = false;
+			Switch.Evaluate(7, new[]
+			{
+				Switch.Case<int>(v => v == 7, (value) => { invoked1 = true; return false; }),
+				Switch.Case<int>(v => v == 7, (value) => { invoked2 = true; return false; }),
+			}).Should().BeTrue();
+			invoked1.Should().BeTrue();
+			invoked2.Should().BeTrue();
+		}
+
+		[Test]
+		public void IntegerTwoCasesFirstAndSecondMatchingAndReturningTrue_Should_InvokeFirstCallbackOnlyAndReturnTrue()
+		{
+			var invoked1 = false;
+			var invoked2 = false;
+			Switch.Evaluate(7, new[]
+			{
+				Switch.Case<int>(v => v == 7, (value) => { invoked1 = true; return true; }),
+				Switch.Case<int>(v => v == 7, (value) => { invoked2 = true; return true; }),
+			}).Should().BeTrue();
+			invoked1.Should().BeTrue();
+			invoked2.Should().BeFalse();
+		}
+
+		[Test]
+		public void SwitchCaseWithNullPredicate_ShouldThrow_ArgumentNullException()
+		{
+			Assert.Throws(typeof(ArgumentNullException), () => { Switch.Case<int>(null, (v) => { return true; }); });
+		}
+
+		[Test]
+		public void SwitchCaseWithNullCallback_ShouldThrow_ArgumentNullException()
+		{
+			Assert.Throws(typeof(ArgumentNullException), () => { Switch.Case<int>(v => v == 7, null); });
+		}
+	}
+}
---
+++ src/Foundations/Foundations/Flow/Switch.cs
@@ -0,0 +1,108 @@
+﻿namespace Elements.Foundations.Flow
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	/// <summary>
+	/// Implements an extended switch-case with callbacks.
+	/// </summary>
+	public static class Switch
+	{
+		/// <summary>
+		/// Evaluates the whole switch construct.
+		/// </summary>
+		/// <typeparam name="T">The type of the value under examination.</typeparam>
+		/// <param name="value">The value under examination.</param>
+		/// <param name="cases">The cases of <paramref name="value"/> that can be handled.</param>
+		/// <returns><c>true</c> if any of the cases had been executed, <c>false</c> otherwise.</returns>
+		public static bool Evaluate<T>(T value, IEnumerable<SwitchCase<T>> cases)
+		{
+			if ((cases == null) || (!cases.Any()))
+			{
+				return false;
+			}
+
+			bool anyInvoked = false;
+
+			foreach (var c in cases)
+			{
+				if (c.Predicate(value))
+				{
+					anyInvoked = true;
+					if (c.Callback(value))
+					{
+						break;
+					}
+				}
+			}
+			
+			return anyInvoked;
+		}
+
+		/// <summary>
+		/// Creates a new <see cref="Elements.Foundations.Flow.SwitchCase{T}"/>
+		/// </summary>
+		/// <typeparam name="T">The type of the value under examination.</typeparam>
+		/// <param name="predicate">The case's predicate.</param>
+		/// <param name="callback">The case's callback to invoke when the predicate is
+		///		fulfilled. If null, a default callback returning true will be used.</param>
+		/// <returns>A case.</returns>
+		public static SwitchCase<T> Case<T>(Predicate<T> predicate, CaseCallback<T> callback)
+		{
+			return new SwitchCase<T>(predicate, callback);
+		}
+
+		#region Nested types
+
+		/// <summary>
+		/// The delegate defining a callback when a switch case matches.
+		/// </summary>
+		/// <typeparam name="T">The type of the value under examination.</typeparam>
+		/// <param name="value">The value under examination.</param>
+		/// <returns>true if further matching cases must not be executed, false
+		///		to "fall through" and evaluate possible further cases.</returns>
+		public delegate bool CaseCallback<T>(T value);
+
+		/// <summary>
+		/// Represents a case.
+		/// </summary>
+		/// <typeparam name="T">The type of the value under examination.</typeparam>
+		public class SwitchCase<T>
+		{
+			/// <summary>
+			/// Gets the case's predicate.
+			/// </summary>
+			internal Predicate<T> Predicate { get; private set; }
+
+			/// <summary>
+			/// Gets the case's callback to invoke when the predicate is fulfilled.
+			/// </summary>
+			internal CaseCallback<T> Callback { get; private set; }
+
+			/// <summary>
+			/// Initializes a new instance of the <see cref="Switch{T}.SwitchCase{T}"/> class.
+			/// </summary>
+			/// <param name="predicate">The case's predicate.</param>
+			/// <param name="callback">The case's callback to invoke when the predicate is
+			///		fulfilled. If null, a default callback returning true will be used.</param>
+			public SwitchCase(Predicate<T> predicate, CaseCallback<T> callback)
+			{
+				if (predicate == null)
+				{
+					throw new ArgumentNullException(nameof(predicate));
+				}
+
+				if (callback == null)
+				{
+					throw new ArgumentNullException(nameof(callback));
+				}
+
+				this.Predicate = predicate;
+				this.Callback = callback;
+			}
+		}
+
+		#endregion
+	}
+}
