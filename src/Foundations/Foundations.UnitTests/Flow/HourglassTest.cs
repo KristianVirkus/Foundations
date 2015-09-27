7cca5f22593c40f86253b7f097584e4ccb1a6cb1
Kristian Virkus <kristian.virkus@gmail.com>
2015-09-27 13:56:44 +0200

    Implementation of the Hourglass class which is some kind of a countdown implementation or a reverse stopwatch respectively.

---
+++ src/Foundations/Foundations.UnitTests/Flow/HourglassTest.cs
@@ -0,0 +1,278 @@
+﻿namespace Elements.Foundations.UnitTests.Flow
+{
+	using System;
+	using System.Threading;
+	using Elements.Foundations.Flow;
+	using FluentAssertions;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class HourglassTest
+	{
+		[Test]
+		public void NotStarted_Should_SignalNotRunningAndNotTimedOutAndRemainingTimeEqualsInitialTimeAndElapsedTimeIsNull()
+		{
+			var hg = new Hourglass(TimeSpan.FromSeconds(5));
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromSeconds(5));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			Thread.Sleep(100);
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromSeconds(5));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+		}
+
+		[Test]
+		public void StartedWith100msTimeout_Should_SignalNotRunningAndTimedOutAndRemainingTimeZeroAndElapsedTimeLarger100ms()
+		{
+			var hg = new Hourglass(TimeSpan.FromMilliseconds(100));
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMilliseconds(100));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			hg.Start();
+
+			hg.IsRunning.Should().BeTrue();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().BeGreaterOrEqualTo(TimeSpan.FromMilliseconds(0));
+
+			Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeTrue();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMilliseconds(0));
+			hg.ElapsedTime.Should().BeGreaterOrEqualTo(TimeSpan.FromMilliseconds(100));
+		}
+
+		[Test]
+		public void AfterStoppingRunningHourglass_Should_KeepStates()
+		{
+			var hg = new Hourglass(TimeSpan.FromMinutes(1));
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMinutes(1));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			hg.Start();
+
+			hg.IsRunning.Should().BeTrue();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().BeGreaterOrEqualTo(TimeSpan.FromSeconds(59));
+
+			hg.Stop();
+			var remaining = hg.RemainingTime;
+			var elapsed = hg.ElapsedTime;
+
+			Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(remaining);
+			hg.ElapsedTime.Should().Be(elapsed);
+		}
+
+		// infinite timeout should never elapse
+		[Test]
+		public void StartedAndStoppedWithInfiniteTimeout_Should_NeverTimeoutAndKeepRemainingTimeInfiniteButUpdateElapsedTime()
+		{
+			var hg = new Hourglass(Timeout.InfiniteTimeSpan);
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(Timeout.InfiniteTimeSpan);
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			hg.Start();
+
+			hg.IsRunning.Should().BeTrue();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().BeGreaterOrEqualTo(Timeout.InfiniteTimeSpan);
+
+			Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+			hg.IsRunning.Should().BeTrue();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(Timeout.InfiniteTimeSpan);
+			hg.ElapsedTime.Should().BeGreaterOrEqualTo(TimeSpan.FromMilliseconds(100));
+
+			hg.Stop();
+			Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(Timeout.InfiniteTimeSpan);
+			hg.ElapsedTime.Should().BeLessThan(TimeSpan.FromMilliseconds(200));
+		}
+
+		[Test]
+		public void ResetBeforeStartInvoked_Should_NotChangeStates()
+		{
+			var hg = new Hourglass(Timeout.InfiniteTimeSpan);
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(Timeout.InfiniteTimeSpan);
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			hg.Reset();
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(Timeout.InfiniteTimeSpan);
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+		}
+
+		// reset to time before ever running
+		[Test]
+		public void ResetToDifferentTimeBeforeStartInvoked_Should_NotChangeStatesButTimeout()
+		{
+			var hg = new Hourglass(Timeout.InfiniteTimeSpan);
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(Timeout.InfiniteTimeSpan);
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			hg.Reset(TimeSpan.FromSeconds(5));
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromSeconds(5));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+		}
+
+		[Test]
+		public void ResetWhileRunning_Should_StopAndResetElapsedAndRemainingTimes()
+		{
+			var hg = new Hourglass(TimeSpan.FromMilliseconds(200));
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMilliseconds(200));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			hg.Start();
+			Thread.Sleep(TimeSpan.FromMilliseconds(50));
+
+			hg.Reset();
+
+			Thread.Sleep(TimeSpan.FromMilliseconds(50));
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMilliseconds(200));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+		}
+
+		[Test]
+		public void ResetToDifferentTimeWhileRunning_Should_StopAndResetElapsedAndRemainingTimes()
+		{
+			var hg = new Hourglass(TimeSpan.FromMilliseconds(200));
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMilliseconds(200));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			hg.Start();
+			Thread.Sleep(TimeSpan.FromMilliseconds(50));
+
+			hg.Reset(TimeSpan.FromMilliseconds(500));
+
+			Thread.Sleep(TimeSpan.FromMilliseconds(50));
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMilliseconds(500));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+		}
+
+		[Test]
+		public void ResetWhileStopped_Should_StopAndResetElapsedAndRemainingTimes()
+		{
+			var hg = new Hourglass(TimeSpan.FromMilliseconds(200));
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMilliseconds(200));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			hg.Start();
+			Thread.Sleep(TimeSpan.FromMilliseconds(50));
+			hg.Stop();
+
+			hg.Reset();
+
+			Thread.Sleep(TimeSpan.FromMilliseconds(50));
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMilliseconds(200));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+		}
+
+		[Test]
+		public void ResetToDifferentTimeWhileStopped_Should_StopAndResetElapsedAndRemainingTimes()
+		{
+			var hg = new Hourglass(TimeSpan.FromMilliseconds(200));
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMilliseconds(200));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			hg.Start();
+			Thread.Sleep(TimeSpan.FromMilliseconds(50));
+			hg.Stop();
+
+			hg.Reset(TimeSpan.FromMilliseconds(500));
+
+			Thread.Sleep(TimeSpan.FromMilliseconds(50));
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeFalse();
+			hg.RemainingTime.Should().Be(TimeSpan.FromMilliseconds(500));
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+		}
+
+		[Test]
+		public void CreateInstanceWithMinus2Milliseconds_ShouldThrow_ArgumentOutOfRangeException()
+		{
+			Assert.Throws(typeof(ArgumentOutOfRangeException), () => { new Hourglass(TimeSpan.FromMilliseconds(-2)); });
+		}
+
+		[Test]
+		public void ResetWithMinus2Milliseconds_ShouldThrow_ArgumentOutOfRangeException()
+		{
+			var hg = new Hourglass(TimeSpan.FromSeconds(1));
+			Assert.Throws(typeof(ArgumentOutOfRangeException), () => { hg.Reset(TimeSpan.FromMilliseconds(-2)); });
+		}
+
+		[Test]
+		public void CreateInstanceWith0Timeout_Should_ImmediatelySignalTimeout()
+		{
+			var hg = new Hourglass(TimeSpan.Zero);
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeTrue();
+			hg.RemainingTime.Should().Be(TimeSpan.Zero);
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+
+			hg.Start();
+			Thread.Sleep(50);
+
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeTrue();
+			hg.RemainingTime.Should().Be(TimeSpan.Zero);
+			hg.ElapsedTime.Should().BeGreaterOrEqualTo(TimeSpan.FromMilliseconds(50));
+		}
+
+		[Test]
+		public void TestForTimeoutInTimeoutState_ShouldThrow_TimeoutException()
+		{
+			var hg = new Hourglass(TimeSpan.Zero);
+			hg.IsRunning.Should().BeFalse();
+			hg.IsTimeout.Should().BeTrue();
+			hg.RemainingTime.Should().Be(TimeSpan.Zero);
+			hg.ElapsedTime.Should().Be(TimeSpan.Zero);
+			Assert.Throws(typeof(TimeoutException), () => { hg.TestForTimeout(); });
+		}
+	}
+}
---
+++ src/Foundations/Foundations/Flow/Hourglass.cs
@@ -0,0 +1,208 @@
+﻿namespace Elements.Foundations.Flow
+{
+	using System;
+	using System.Diagnostics;
+	using System.Threading;
+	using Elements.Foundations.Synchronization;
+
+	/// <summary>
+	/// Implements a countdown stopwatch which can be used e.g.
+	/// to handle timeouts over multiple single actions which
+	/// require their own timeout. It uses the
+	/// <see cref="System.Diagnostics.Stopwatch"/> class for
+	/// time measurement as accurate as possible.
+	/// </summary>
+	public class Hourglass
+	{
+		#region Fields
+
+		private ReaderWriterLockSlim syncRoot = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
+		private Stopwatch stopWatch = new Stopwatch();
+		private bool internalIsRunning = false;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the initial timeout.
+		/// </summary>
+		public TimeSpan Timeout { get; private set; }
+
+		/// <summary>
+		/// Gets the remaining time until a timeout occurs. If the
+		/// timeout had been set to infinity, the set up timeout is returned.
+		/// </summary>
+		public TimeSpan RemainingTime
+		{
+			get
+			{
+				using (this.syncRoot.Read())
+				{
+					if (this.Timeout == System.Threading.Timeout.InfiniteTimeSpan)
+					{
+						return this.Timeout;
+					}
+					else
+					{
+						TimeSpan remaining = this.Timeout - this.stopWatch.Elapsed;
+						return TimeSpan.FromTicks(Math.Max(0, remaining.Ticks));
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets whether the hourglass is currently running.
+		/// </summary>
+		public bool IsRunning
+		{
+			get
+			{
+				// The hourglass is said to be running if the Start
+				// method had been called (internalIsRunning) and
+				// the set up timeout is either infinite or has
+				// not elapsed yet.
+				return (this.internalIsRunning)
+					&& ((this.Timeout == System.Threading.Timeout.InfiniteTimeSpan)
+						|| (this.stopWatch.Elapsed < this.Timeout));
+			}
+		}
+
+		/// <summary>
+		/// Gets whether a timeout has occurred. If the timeout had been
+		/// specified with -1 milliseconds the value of this property
+		/// is always <c>false</c>.
+		/// </summary>
+		public bool IsTimeout
+		{
+			get
+			{
+				return (this.Timeout != System.Threading.Timeout.InfiniteTimeSpan)
+					&& (this.stopWatch.Elapsed >= this.Timeout);
+			}
+		}
+
+		/// <summary>
+		/// Gets the time that has elapsed since the Start method was
+		/// called. Times while stopped are not included.
+		/// </summary>
+		public TimeSpan ElapsedTime
+		{
+			get
+			{
+				return this.stopWatch.Elapsed;
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Starts the hourglass.
+		/// </summary>
+		/// <returns>Returns this instance to enable a one-liner
+		///		instantiation and starting.</returns>
+		public Hourglass Start()
+		{
+			using (this.syncRoot.Write())
+			{
+				this.stopWatch.Start();
+				this.internalIsRunning = true;
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Stops the hourglass.
+		/// </summary>
+		public void Stop()
+		{
+			using (this.syncRoot.Write())
+			{
+				this.stopWatch.Stop();
+				this.internalIsRunning = false;
+			}
+		}
+
+		/// <summary>
+		/// Resets the hourglass to the initially set up timeout.
+		/// If the hourglass is running upon invocation, it is
+		/// stopped first.
+		/// </summary>
+		/// <returns>Returns this instance to enable a one-liner
+		///		instantiation and starting.</returns>
+		public Hourglass Reset()
+		{
+			using (this.syncRoot.Write())
+			{
+				this.Stop();
+				this.stopWatch.Reset();
+				return this;
+			}
+		}
+
+		/// <summary>
+		/// Resets the hourglass to an arbitrary timeout.
+		/// If the hourglass is running upon invocation, it is
+		/// stopped first.
+		/// </summary>
+		/// <param name="timeout">Time larger or equal 1 millisecond until the
+		///		timeout occurs.	Use -1 milliseconds for an infinite timeout.
+		///		If the initial timeout is zero, the timeout occurs immediately.</param>
+		/// <returns>Returns this instance to enable a one-liner
+		///		instantiation and starting.</returns>
+		/// <exception cref="System.ArgumentOutOfRangeException">Thrown if <paramref name="timeout"/> is less than -1 milliseconds.</exception>
+		public Hourglass Reset(TimeSpan timeout)
+		{
+			if (timeout < System.Threading.Timeout.InfiniteTimeSpan)
+			{
+				throw new ArgumentOutOfRangeException(nameof(timeout));
+			}
+
+			using (this.syncRoot.Write())
+			{
+				this.Reset();
+				this.Timeout = timeout;
+				return this;
+			}
+		}
+
+		/// <summary>
+		/// Tests whether the time has elapsed and a timeout has occurred. If
+		/// that is true, an <see cref="System.TimeoutException"/> is thrown
+		/// otherwise nothing happens.
+		/// </summary>
+		public void TestForTimeout()
+		{
+			if (this.IsTimeout)
+			{
+				throw new TimeoutException();
+			}
+		}
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Hourglass"/> class.
+		/// </summary>
+		/// <param name="timeout">Time larger or equal 1 millisecond until the
+		///		timeout occurs.	Use -1 milliseconds for an infinite timeout.
+		///		If the initial timeout is zero, the timeout occurs immediately.</param>
+		/// <exception cref="System.ArgumentOutOfRangeException">Thrown if <paramref name="timeout"/> is less than -1 milliseconds.</exception>
+		public Hourglass(TimeSpan timeout)
+		{
+			if (timeout < System.Threading.Timeout.InfiniteTimeSpan)
+			{
+				throw new ArgumentOutOfRangeException(nameof(timeout));
+			}
+
+			this.Timeout = timeout;
+		}
+
+		#endregion
+	}
+}
